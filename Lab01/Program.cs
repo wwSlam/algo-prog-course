9b814b06e15e51c081e31fbfec295e898eb5ff6d
Maksim Podlobkov <pvmmach@gmail.com>
2026-09-16 17:45:39 +0300

    Lab01: arithmetic with variables

---
+++ Lab01/Program.cs
@@ -0,0 +1,55 @@
+﻿string myName = "Максим Подлобков";
+string groupName = "ИСП-252";
+int courseNumber = 2;
+double averageGrade = 4.6;
+bool isBudget = true;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное мест{isBudget}");
+
+
+Console.WriteLine();
+Console.WriteLine("Ремонт: комната");
+
+double roomWidth = 3.5;
+double roomLength = 4.2;
+
+double roomArea = roomWidth * roomLength;
+double roomPerimeter = (roomWidth + roomLength) * 2;
+
+Console.WriteLine($"Ширина: {roomWidth} м, длина: {roomLength} м");
+Console.WriteLine($"Площадь: {roomArea} кв.м");
+Console.WriteLine($"Периметр: {roomPerimeter} м");
+
+
+
+Console.WriteLine();
+Console.WriteLine("Покупка ноутбука в рассрочку");
+
+int laptopPrice = 65000;
+int monthsCount = 12;
+double interestRate = 0.08;
+
+double totalWithInterest = laptopPrice * (1 + interestRate);
+double monthlyPayment = totalWithInterest / monthsCount;
+
+Console.WriteLine($"Цена ноутбука: {laptopPrice} руб.");
+Console.WriteLine($"Итого с процентами: {totalWithInterest} руб.");
+Console.WriteLine($"Платёж в месяц: {monthlyPayment} руб.");
+
+
+Console.WriteLine();
+Console.WriteLine("Внимание: деление int");
+
+int totalStudents = 25;
+int groupsCount = 4;
+
+int studentsPerGroupWrong = totalStudents / groupsCount;
+double studentsPerGroupCorrect = (double)totalStudents / groupsCount;
+
+Console.WriteLine($"25 / 4 как int:    {studentsPerGroupWrong}");
+Console.WriteLine($"25 / 4 как double: {studentsPerGroupCorrect}");
