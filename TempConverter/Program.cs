99ad6355a5f7a7b79b3e961319aea8eba6edbe96
Maksim Podlobkov <pvmmach@gmail.com>
2026-09-16 19:12:19 +0300

    Lab01 task 3:modulo operator practice

---
+++ TempConverter/Program.cs
@@ -0,0 +1,6 @@
+﻿double celsius = 42.2;
+const double tr = 273.15;
+double farengeit = celsius * (9/5) + 32;
+double Kelv = celsius + tr;
+
+Console.WriteLine($"{celsius}C = {farengeit}F = {Kelv}K");
